3457368e0ffcf45a58488f847043fb49b5b89b66
vledd <nechaev1701@gmail.com>
2024-03-21 14:26:52 +0200

    PaintCrop: Added additional helper function

--- src/main.py
+++ src/main.py
@@ -69,11 +69,25 @@ def count_images_qty(src_tileset: Image,
     return x_tiles_qty, y_tiles_qty
 
 
-# TODO it would be also nice to make same function but not by number but by (x, y) of image. Like: (1, 0) -> (54, 0)
-def get_image_coords_by_no(tileset: Image,
-                           image_no: int,
-                           img_tile_xy_qty: tuple[int, int],
-                           tiles_order: TilesetTilesOrder) -> int or tuple[int, int]:
+def get_image_coords_px_by_tile_coords(tileset: Image,
+                                       img_tile_coords: tuple[int, int],
+                                       img_tile_xy_qty: tuple[int, int]) -> int or tuple[int, int]:
+
+    max_image_qty: tuple[int, int] = count_images_qty(tileset, img_tile_xy_qty)
+    if img_tile_coords[0] not in range(0, max_image_qty[0]) or\
+            img_tile_coords[1] not in range(0, max_image_qty[1]):
+        return -1
+
+    x_coord_px: int = img_tile_coords[0] * (TILE_SIZE_PX * img_tile_xy_qty[0] + (2 * img_tile_xy_qty[0]))
+    y_coord_px: int = img_tile_coords[1] * (TILE_SIZE_PX * img_tile_xy_qty[1] + (2 * img_tile_xy_qty[1]))
+
+    return x_coord_px, y_coord_px
+
+
+def get_image_coords_px_by_no(tileset: Image,
+                              image_no: int,
+                              img_tile_xy_qty: tuple[int, int],
+                              tiles_order: TilesetTilesOrder) -> int or tuple[int, int]:
     """
 
     :param tileset:
@@ -84,7 +98,7 @@ def get_image_coords_by_no(tileset: Image,
     """
 
     max_image_no: int = count_images_qty(tileset, img_tile_xy_qty)[0] * count_images_qty(tileset, img_tile_xy_qty)[1]
-    
+
     # TODO we can add additional argument such as "content aware".
     # TODO If true, do not include blank images as a possible values to address.
 
@@ -146,7 +160,8 @@ def is_selected_zone_empty(tileset: Image,
 # tiles = Image.open("./Tiles_240.png")
 # print(is_selected_zone_empty(tiles, (280, 1900), (32, 16),))
 # print(count_images_qty(tiles, (3, 3)))
-# get_image_coords_by_no(tiles, 1, (3, 3), TilesetTilesOrder.LEFT2RIGHT)
+# get_image_coords_px_by_no(tiles, 1, (3, 3), TilesetTilesOrder.LEFT2RIGHT)
+# print(get_image_coords_px_by_tile_coords(tiles, (2, 1), (3, 3)))
 
 # To be refactored, for testing please uncomment
 # src = Image.open("resources/pic/pic1.jpg")
